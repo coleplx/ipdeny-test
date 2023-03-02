9eb1af3210e78d6a7fc739895c5b90ff51624056
Paulo Paracatu <pauloparacatu@kinsta.com>
2023-03-01 22:54:37 -0300

    fix deploy

--- app.js
+++ app.js
@@ -2,45 +2,38 @@ const http = require('http');
 const url = require('url');
 const redis = require('redis');
 
-// Redis connection information
-const redisUrl = 'redis://:t5iKM7G1jJjTUV0i@ipdenyresearch-vd9xw-redis-master.ipdenyresearch-vd9xw.svc.cluster.local:6379/0';
-
-// Create a Redis client
-const redisClient = redis.createClient(redisUrl);
-
-// Create an HTTP server
-const server = http.createServer((req, res) => {
-  // Parse the query parameters from the request URL
-  const { query } = url.parse(req.url, true);
-  const env = query.env;
-
-  if (!env) {
-    res.statusCode = 400;
-    res.end('Missing "env" parameter');
-  } else {
-    // Retrieve the value of the "env" key from Redis
-    redisClient.get(env, (err, result) => {
-      if (err) {
-        res.statusCode = 500;
-        res.end(`Error retrieving value from Redis: ${err}`);
-      } else {
-        // Parse the Redis value as JSON
-        let jsonResult;
-        try {
-          jsonResult = JSON.parse(result);
-        } catch (e) {
-          jsonResult = result;
-        }
-        // Send the JSON response
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify(jsonResult));
-      }
-    });
-  }
+// Redis connection details
+const dbHost = process.env.DB_HOST;
+const dbPort = process.env.DB_PORT;
+const dbName = process.env.DB_NAME;
+const dbPassword = process.env.DB_PASSWORD;
+const redisClient = redis.createClient({
+  host: dbHost,
+  port: dbPort,
+  password: dbPassword,
+  db: dbName,
 });
 
-// Start the HTTP server
-const port = process.env.PORT || 3000;
-server.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+// Create the HTTP server
+const server = http.createServer((req, res) => {
+  // Parse the URL to get the "env" query parameter
+  const queryObject = url.parse(req.url, true).query;
+  const env = queryObject.env;
+
+  // Retrieve values from the Redis list using LRANGE
+  redisClient.lrange(env, 0, -1, (err, result) => {
+    if (err) {
+      res.statusCode = 500;
+      res.end(`Error retrieving value from Redis: ${err}`);
+    } else {
+      // Send the JSON response
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify(result));
+    }
+  });
+});
+
+// Start the server
+server.listen(3000, () => {
+  console.log('Server running on port 3000');
 });
